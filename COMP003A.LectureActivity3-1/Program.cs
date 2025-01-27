545694930763b12f92963b1ee1df65f0ff818399
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-01-27 14:37:15 -0800

    Add project files.

---
+++ COMP003A.LectureActivity3-1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+// Author : Isaac Martinez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Demonstrate selection statements in C#.
+*/
+namespace COMP003A.LectureActivity3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
